a8bf223f978ca909d2b389a9afb8f5bff8aa56dc
bryancarrion19 <bryancarrion791@gmail.com>
2025-01-02 22:21:47 +0100

    Vista contacto

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
-import MainLayout from '../layouts/MainLayout';
+import React from "react";
+import Navbar from "../components/Navbar";
+import MainLayout from "../layouts/MainLayout";
+import FormularioContacto from "../components/FormularioContacto";
 
 const Contacto = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Lógica para manejar el envío
+  };
+
   return (
     <MainLayout>
-      <Navbar activeLink="Contacto"/>
+      <Navbar activeLink="Contacto" />
+      <section>
+        <div>
+          <div>
+            <div>
+              <FormularioContacto
+                name="nombre"
+                email="email"
+                phone="telefono"
+                motivo="motivo"
+                enviar={handleSubmit}
+              />
+            </div>
+          </div>
+        </div>
+      </section>
     </MainLayout>
   );
 };
