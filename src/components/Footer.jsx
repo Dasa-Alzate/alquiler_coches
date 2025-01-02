b40909dc9ed7d0ee402b77d582c5e57bc5572d1c
Dasa-Alzate <david.s356811@cesurformacion.com>
2025-01-02 22:22:41 +0100

    correccion de typo en propiedades allowFullScreen y referrerPolicy

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -27,7 +27,7 @@ const Footer = ({ activeLink, isLogged }) => {
 
       </div>
       <div className="flex">
-        <iframe className='w-96 h-60' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6287.403874143237!2d-1.1500007247731776!3d38.00741227192753!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd63819baae4321b%3A0x7a6261663713684!2sCESUR%20Murcia%20Audiovisual%20Formaci%C3%B3n%20Profesional!5e0!3m2!1ses!2ses!4v1735749095476!5m2!1ses!2ses" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe className='w-96 h-60' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6287.403874143237!2d-1.1500007247731776!3d38.00741227192753!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd63819baae4321b%3A0x7a6261663713684!2sCESUR%20Murcia%20Audiovisual%20Formaci%C3%B3n%20Profesional!5e0!3m2!1ses!2ses!4v1735749095476!5m2!1ses!2ses" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
       </div>
 
     </footer>
